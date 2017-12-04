935eadc920a0620a25dad5657ed8251ec65deb4a
Saquib08 <er.saquib08@gmail.com>
2017-12-04 16:21:35 +0000

    Server Code for sending commit referenc to client

---
+++ src/com/cyclometic/server/CyclomaticServer.java
@@ -0,0 +1,171 @@
+package com.cyclometic.server;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Stack;
+
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.errors.CorruptObjectException;
+import org.eclipse.jgit.errors.IncorrectObjectTypeException;
+import org.eclipse.jgit.errors.MissingObjectException;
+import org.eclipse.jgit.lib.Constants;
+import org.eclipse.jgit.lib.ObjectId;
+import org.eclipse.jgit.lib.ObjectLoader;
+import org.eclipse.jgit.lib.Ref;
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.eclipse.jgit.revwalk.RevTree;
+import org.eclipse.jgit.revwalk.RevWalk;
+import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
+import org.eclipse.jgit.treewalk.TreeWalk;
+import javax.xml.bind.annotation.XmlElement; 
+import javax.xml.bind.annotation.XmlRootElement; 
+import javax.ws.rs.GET; 
+import javax.ws.rs.Path; 
+import javax.ws.rs.Produces; 
+import javax.ws.rs.core.MediaType;  
+
+@Path("/CyclometicServer") 
+public class CyclomaticServer {
+  
+	private static final String REMOTE_URL = "https://github.com/sack0809/ScalableComputing.git";
+	   
+	  // @GET 
+	   //@Path("/repo") 
+	   //@Produces(MediaType.TEXT_PLAIN) 
+	
+    public void getRepo() throws IOException , GitAPIException{
+    	
+    	 try (Git result = Git.cloneRepository()
+                 .setURI(REMOTE_URL)
+                 .setDirectory(new File("/Users/playsafe/Desktop/Java/Assignment2/src/repo"))
+                  .setCloneAllBranches(true)
+                  
+                 .call()) {
+ 	        // Note: the call() returns an opened repository already which needs to be closed to avoid file handle leaks!
+ 	        System.out.println("Having repository: " + result.getRepository().getDirectory());
+ 	        
+         }
+    	
+    	
+    	
+   /* 	FileRepositoryBuilder builder = new FileRepositoryBuilder();
+        Repository repository = builder
+                .setGitDir(new File("/Users/playsafe/Desktop/Java/Assignment2/src/repo/.git")).readEnvironment()
+                .findGitDir().build();*/
+
+        //listRepositoryContents(repository);
+
+        //repository.close();
+		
+    	
+    	}
+    
+	@SuppressWarnings("deprecation")
+	
+	 @GET 
+	   @Path("/main") 
+	 @Produces(MediaType.APPLICATION_JSON)
+	public String listCommits() throws MissingObjectException, IncorrectObjectTypeException, CorruptObjectException, IOException, GitAPIException {
+		// TODO Auto-generated method stub
+		Stack<String> commList = new Stack<String>();
+        FileRepositoryBuilder builder = new FileRepositoryBuilder();
+		        Repository repository = builder
+		                .setGitDir(new File("/Users/playsafe/Desktop/Java/Assignment2/src/repo/.git")).readEnvironment()
+		                .findGitDir().build();
+		PrintStream out = new PrintStream(new FileOutputStream("OutFile.txt"));
+		@SuppressWarnings("deprecation")
+		Git git = new Git(repository);
+		Ref head = repository.getRef("HEAD");
+
+        // a RevWalk allows to walk over commits based on some filtering that is defined
+        RevWalk walk = new RevWalk(repository);
+
+        
+        List<Ref> branches = git.branchList().call();
+        for (Ref branch : branches) {
+            String branchName = branch.getName();
+
+            System.out.println("Commits of branch: " + branch.getName());
+            System.out.println("-------------------------------------");
+
+            Iterable<RevCommit> commits = git.log().all().call();
+
+            for (RevCommit commit : commits) {
+                boolean foundInThisBranch = false;
+
+                RevCommit targetCommit = walk.parseCommit(repository.resolve(
+                        commit.getName()));
+                
+                for (Map.Entry<String, Ref> e : repository.getAllRefs().entrySet()) {
+                    if (e.getKey().startsWith(Constants.R_HEADS)) {
+                        if (walk.isMergedInto(targetCommit, walk.parseCommit(
+                                e.getValue().getObjectId()))) {
+                            String foundInBranch = e.getValue().getName();
+                            if (branchName.equals(foundInBranch)) {
+                                foundInThisBranch = true;
+                                RevTree tree = targetCommit.getTree();
+                                TreeWalk treeWalk = new TreeWalk(repository);
+                                treeWalk.addTree(tree);
+                                treeWalk.setRecursive(false);
+                                //out.println("Commit found: "+ commit.getName());
+                                while (treeWalk.next()) {
+                                  // out.println("found: " +treeWalk.getPathString());
+                                   
+                                   //ObjectId 	
+                                   //ObjectLoader loader = repository.open(objectId);
+                                   //loader.copyTo(System.out);
+                                    
+                                }
+                                break;
+                            }
+                        }
+                    }
+                }
+
+                if (foundInThisBranch) {
+                  	commList.add(commit.getName());
+                    //System.out.println(commit.getName());
+                    //System.out.println(commit.getAuthorIdent().getName());
+                    //System.out.println(new Date(commit.getCommitTime()));
+                    //System.out.println(commit.getFullMessage());
+                }
+            }
+       
+
+        
+        
+    
+        
+       
+    }
+        repository.close();
+		return commList.pop();
+	}
+	
+	 //@GET 
+	   //@Path("/main") 
+	// @Produces(MediaType.APPLICATION_XML)
+	/*public String test() throws IOException, GitAPIException
+    {
+    	
+		getRepo();
+    	String commitList = listCommits();
+    	System.out.println("Commit found:");
+    	for (String commit : commitList) 
+    	{
+    	    System.out.println(commit);    	
+    	}
+		return "<Commit>" + commitList +"</Commit>";
+    	
+    
+    }
+    
+*/    
+}
+        
